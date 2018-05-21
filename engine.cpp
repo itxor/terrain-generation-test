f8b14f00f01aa00ccdbbe8d8cee112e94c16d25e
unknown <itxor@yandex.ru>
2018-05-22 01:03:19 +0700

    Рефакторинг engine.cpp

--- engine.cpp
+++ engine.cpp
@@ -3,17 +3,81 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stb_image.h>
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
 #include "Shader.h"
 #include "Camera.h"
 #include "consts.h"
 #include "CallBacks.h"
 
 using namespace std;
+
+/*function prototipes*/
+void initialGLFWEnviroment();
+GLFWwindow * createWindow(int width, int height, string title);
+void initialGLEW();
+void do_movement();
+void displayClear();
+void bufferingInitialization(GLuint &VAO, GLuint &positions, GLuint &indices);
+void uniformValuesUpload(Shader mainShader);
+
+int main()
+{
+	/*variables section*/
+	map<unsigned int, const GLchar *> shaderNames = {
+		{ 1, "shaders/shader.vs" },
+		{ 2, "shaders/shader.tcs" },
+		{ 3, "shaders/shader.tes" },
+		{ 4, "shaders/shader.gmt" },
+		{ 5, "shaders/shader.frag" }
+	};
+	GLuint VAO,
+		positions,
+		indices;
+	GLFWwindow * window;
+	/*variables sections*/
+
+	/*initial's section*/
+	initialGLFWEnviroment();
+	window = createWindow(WIDTH, HEIGHT, "hello, triangulation");
+	if (window == nullptr)
+	{
+		return 1;
+	}
+	else
+	{
+		initialGLEW();
+	}
+	enableCallBackFunctions(window);
+	bufferingInitialization(VAO, positions, indices);
+	/*initial's section*/
+	
+	Shader mainShader(shaderNames);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		do_movement();
+		displayClear();
+		mainShader.Use();
+
+		uniformValuesUpload(mainShader);
+
+		mainShader.setMat4(
+			"projection", 
+			glm::perspective(glm::radians(camera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f)
+		);
+		mainShader.setMat4("view", camera.GetViewMatrix());
+
+		glBindVertexArray(VAO);
+		mainShader.setMat4("model", glm::mat4(1.0f));
+		glDrawArrays(GL_PATCHES, 0, 3);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		glBindVertexArray(0);
+	}
+	return 0;
+}
 
 void initialGLFWEnviroment()
 {
@@ -49,7 +113,7 @@ void initialGLEW()
 		std::cout << "Ошибка инициализации GLEW" << std::endl;
 		return;
 	}
-	else 
+	else
 	{
 		glViewport(0, 0, WIDTH, HEIGHT);
 		glEnable(GL_DEPTH_TEST);
@@ -79,76 +143,38 @@ void displayClear()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
-int main()
+void bufferingInitialization(GLuint &VAO, GLuint &positions, GLuint &indices)
 {
-	initialGLFWEnviroment();
-	GLFWwindow * window = createWindow(WIDTH, HEIGHT, "hello, triangulation");
-	if (window == nullptr)
+	if (VAO != -1)
 	{
-		return 1;
-	}
-	else
-	{
-		initialGLEW();
-	}
-	
-	enableCallBackFunctions(window);
-
-	map<unsigned int, const GLchar *> shaderNames = {
-		{ 1, "shaders/shader.vs" },
-		{ 2, "shaders/shader.tcs" },
-		{ 3, "shaders/shader.tes" },
-		{ 4, "shaders/shader.gmt" },
-		{ 5, "shaders/shader.frag" }
-	};
-	Shader mainShader(shaderNames);
-
-	GLuint VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	GLuint positions;
-	glGenBuffers(1, &positions);
-	glBindBuffer(GL_ARRAY_BUFFER, positions);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
-	glEnableVertexAttribArray(0);
-	
-	GLuint indices;
-	glGenBuffers(1, &indices);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Faces), Faces, GL_STATIC_DRAW);
-	glBindVertexArray(0);
-
-	while (!glfwWindowShouldClose(window))
-	{
-		do_movement();
-		displayClear();
-		mainShader.Use();
-
-		//загрузка uniform-переменных
-		/*mainShader.setFloat("TessLevelOuter", 1);
-		mainShader.setFloat("TessLevelInner", 2);*/
-
-		mainShader.setVec3("eyePosition", camera.Position);
-		mainShader.setMat3("NormalMatrix", glm::mat3(1.0f));
-
-		mainShader.setVec3("LightPosition", glm::vec3(1.2f, 1.0f, 2.0f));
-		mainShader.setVec3("DiffuseMaterial", glm::vec3(0.61424f, 0.04136f, 0.04136f));
-		mainShader.setVec3("AmbientMaterial", glm::vec3(0.1745f, 0.01175f, 0.01175f));
-
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
-		glm::mat4 view = camera.GetViewMatrix();
-		mainShader.setMat4("projection", projection);
-		mainShader.setMat4("view", view);
-
+		glGenVertexArrays(1, &VAO);
 		glBindVertexArray(VAO);
-		glm::mat4 model(1.0f);
-		mainShader.setMat4("model", model);
-		glDrawArrays(GL_PATCHES, 0, 3);
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+	}
+	if (positions != -1)
+	{
+		glGenBuffers(1, &positions);
+		glBindBuffer(GL_ARRAY_BUFFER, positions);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
+		glEnableVertexAttribArray(0);
+	}
+	if (indices != -1)
+	{
+		glGenBuffers(1, &indices);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Faces), Faces, GL_STATIC_DRAW);
 		glBindVertexArray(0);
 	}
-	return 0;
+	return;
+}
+
+void uniformValuesUpload(Shader mainShader)
+{
+	//загрузка uniform-переменных
+	mainShader.setVec3("eyePosition", camera.Position);
+	mainShader.setMat3("NormalMatrix", glm::mat3(1.0f));
+
+	mainShader.setVec3("LightPosition", glm::vec3(1.2f, 1.0f, 2.0f));
+	mainShader.setVec3("DiffuseMaterial", glm::vec3(0.61424f, 0.04136f, 0.04136f));
+	mainShader.setVec3("AmbientMaterial", glm::vec3(0.1745f, 0.01175f, 0.01175f));
 }
