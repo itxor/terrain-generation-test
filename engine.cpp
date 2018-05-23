c4a9ba2ae8733b0f58ee71e80cadcca8e5ffe7c2
unknown <vdk_v@mail.ru>
2018-05-23 07:30:13 +0700

    проба генерации и тесселяции на сетке

--- engine.cpp
+++ engine.cpp
@@ -10,6 +10,8 @@
 #include "Camera.h"
 #include "consts.h"
 #include "CallBacks.h"
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -19,17 +21,21 @@ GLFWwindow * createWindow(int width, int height, string title);
 void initialGLEW();
 void do_movement();
 void displayClear();
-void bufferingInitialization(GLuint &VAO, GLuint &positions, GLuint &indices);
+void bufferingInitialization(GLuint &VAO, GLuint &positions);
 void uniformValuesUpload(Shader mainShader);
+GLfloat * gridGenerator(int triangleCountInSide);
+
+const int numberOfQuads = 50;
+GLfloat vertices[numberOfQuads * numberOfQuads * 18];
 
 int main()
 {
 	/*variables section*/
 	map<unsigned int, const GLchar *> shaderNames = {
 		{ 1, "shaders/shader.vs" },
-		{ 2, "shaders/shader.tcs" },
-		{ 3, "shaders/shader.tes" },
-		{ 4, "shaders/shader.gmt" },
+		//{ 2, "shaders/shader.tcs" },
+		//{ 3, "shaders/shader.tes" },
+		//{ 4, "shaders/shader.gmt" },
 		{ 5, "shaders/shader.frag" }
 	};
 	GLuint VAO,
@@ -50,11 +56,17 @@ int main()
 		initialGLEW();
 	}
 	enableCallBackFunctions(window);
-	bufferingInitialization(VAO, positions, indices);
+
+	GLfloat * grid = new GLfloat[numberOfQuads * numberOfQuads * 18];
+	grid = gridGenerator(numberOfQuads);
+	for (int i = 0; i < numberOfQuads * numberOfQuads * 18; i++)
+		vertices[i] = grid[i];
+
+	bufferingInitialization(VAO, positions);
 	/*initial's section*/
 	
 	Shader mainShader(shaderNames);
-
+	
 	while (!glfwWindowShouldClose(window))
 	{
 		do_movement();
@@ -63,15 +75,14 @@ int main()
 
 		uniformValuesUpload(mainShader);
 
-		mainShader.setMat4(
-			"projection", 
-			glm::perspective(glm::radians(camera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f)
-		);
+		mainShader.setMat4("projection", 
+			glm::perspective(glm::radians(camera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f));
 		mainShader.setMat4("view", camera.GetViewMatrix());
-
+		
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glBindVertexArray(VAO);
 		mainShader.setMat4("model", glm::mat4(1.0f));
-		glDrawArrays(GL_PATCHES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, numberOfQuads * numberOfQuads * 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 		glBindVertexArray(0);
@@ -143,7 +154,7 @@ void displayClear()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
-void bufferingInitialization(GLuint &VAO, GLuint &positions, GLuint &indices)
+void bufferingInitialization(GLuint &VAO, GLuint &positions)
 {
 	if (VAO != -1)
 	{
@@ -158,13 +169,6 @@ void bufferingInitialization(GLuint &VAO, GLuint &positions, GLuint &indices)
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
 		glEnableVertexAttribArray(0);
 	}
-	if (indices != -1)
-	{
-		glGenBuffers(1, &indices);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Faces), Faces, GL_STATIC_DRAW);
-		glBindVertexArray(0);
-	}
 	return;
 }
 
@@ -172,9 +176,81 @@ void uniformValuesUpload(Shader mainShader)
 {
 	//загрузка uniform-переменных
 	mainShader.setVec3("eyePosition", camera.Position);
-	mainShader.setMat3("NormalMatrix", glm::mat3(1.0f));
+	//mainShader.setMat3("NormalMatrix", glm::mat3(1.0f));
+	//mainShader.setVec3("LightPosition", glm::vec3(1.2f, 1.0f, 2.0f));
+	//mainShader.setVec3("DiffuseMaterial", glm::vec3(0.61424f, 0.04136f, 0.04136f));
+	//mainShader.setVec3("AmbientMaterial", glm::vec3(0.1745f, 0.01175f, 0.01175f));
+}
 
-	mainShader.setVec3("LightPosition", glm::vec3(1.2f, 1.0f, 2.0f));
-	mainShader.setVec3("DiffuseMaterial", glm::vec3(0.61424f, 0.04136f, 0.04136f));
-	mainShader.setVec3("AmbientMaterial", glm::vec3(0.1745f, 0.01175f, 0.01175f));
+GLfloat * gridGenerator(const int triangleCountInSide)
+{
+	float step = 2.0f / (float)(triangleCountInSide);
+	
+	/*генерируем матрицу*/
+	vector<vector<vector<float>>> vertices;
+	vertices.resize(triangleCountInSide);
+
+	for (int i = 0; i < vertices.size(); i++)
+		vertices[i].resize(triangleCountInSide);
+
+	for (int i = 0; i < vertices.size(); i++)
+		for (int j = 0; j < vertices[i].size(); j++)
+			vertices[i][j].resize(18);
+
+	float valueZ = 1.0f;
+	for (int i = 0; i < vertices.size(); i++)
+	{
+		float valueX = -1.0f;
+		for (int j = 0; j < vertices[i].size(); j++)
+		{
+			vector<float> temp(18);
+			/*1 “–≈”√ќЋ№Ќ» */
+			/*1 точка*/
+			temp[0] = valueX;			//x
+			temp[1] = 0;				//y
+			temp[2] = valueZ;			//z
+			/*2 точка*/
+			temp[3] = valueX;			//x
+			temp[4] = 0;				//y
+			temp[5] = valueZ - step;	//z
+			/*3 точка*/
+			temp[6] = valueX + step;	//x
+			temp[7] = 0;				//y
+			temp[8] = valueZ - step;	//z
+			
+			/*2 “–≈”√ќЋ№Ќ» */
+			/*1 точка*/
+			temp[9] = valueX;			//x
+			temp[10] = 0;				//y
+			temp[11] = valueZ;			//z
+			/*2 точка*/
+			temp[12] = valueX + step;	//x
+			temp[13] = 0;				//y
+			temp[14] = valueZ;			//z
+			/*3 точка*/
+			temp[15] = valueX + step;	//x
+			temp[16] = 0;				//y
+			temp[17] = valueZ - step;	//z
+
+			vertices[i][j] = temp;
+
+			valueX += step;
+		}
+		valueZ -= step;
+	}
+	
+	GLfloat * resultVertices = new GLfloat[triangleCountInSide * triangleCountInSide * 18];
+	int counter = 0;
+	for (int i = 0; i < triangleCountInSide; i++)
+	{
+		for (int j = 0; j < triangleCountInSide; j++)
+		{
+			for (int k = 0; k < 18; k++)
+			{
+				resultVertices[counter] = vertices[i][j][k];
+				counter++;
+			}
+		}
+	}
+	return resultVertices;
 }
--- shaders/shader.vs
+++ shaders/shader.vs
@@ -1,9 +1,13 @@
 #version 440 core
 
 layout (location = 0) in vec3 position;
-out vec3 vPosition;
+//out vec4 vPosition;
+
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
 
 void main()
 {
-	vPosition = position;
+	gl_Position = projection * view * model * vec4(position, 1.0f);
 }
